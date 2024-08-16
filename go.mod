98c1a0ceb510e24a984fe5a6b057929adb21ed38
Dominik <wezik@users.noreply.github.com>
2024-08-16 07:28:16 +0200

    SQlite integration

--- go.mod
+++ go.mod
@@ -7,5 +7,6 @@ require (
 	github.com/gorilla/mux v1.8.1 // indirect
 	github.com/gorilla/websocket v1.5.3 // indirect
 	github.com/joho/godotenv v1.5.1 // indirect
+	github.com/mattn/go-sqlite3 v1.14.22 // indirect
 	github.com/nicklaw5/helix/v2 v2.30.0 // indirect
 )
---
+++ pkg/database/db_client.go
@@ -0,0 +1,69 @@
+package database
+
+import (
+	"database/sql"
+	"log"
+	"strings"
+
+	_ "github.com/mattn/go-sqlite3"
+)
+
+const (
+	dbPath        = "./squeel.db"
+	versionQuery  = "SELECT COUNT(*) FROM queries"
+	versionInsert = "INSERT INTO queries (id, query) VALUES (?, ?)"
+)
+
+func Init() *sql.DB {
+	log.Println("Initializing database...")
+	db, err := sql.Open("sqlite3", dbPath)
+	if err != nil {
+		log.Println(err)
+	}
+	log.Println("Database initialized")
+	err = runQueries(db)
+	if err != nil {
+		log.Println(err)
+	}
+	return db
+}
+
+func runQueries(db *sql.DB) error {
+	var count int = 0
+	err := db.QueryRow(versionQuery).Scan(&count)
+	if err != nil {
+		count = 0
+	}
+
+	if count != len(queries) {
+		log.Println("Updating database...")
+	}
+
+	for i := count; i < len(queries); i++ {
+		query := strings.ReplaceAll(queries[i], "\n", "")
+		query = strings.Join(strings.Fields(query), " ")
+		log.Println("Running query", query)
+		_, err := db.Exec(query)
+		if err != nil {
+			return err
+		}
+		_, err = db.Exec(versionInsert, i, query)
+		if err != nil {
+			return err
+		}
+	}
+
+	log.Println("Database is up to date")
+	return nil
+}
+
+var queries = []string{
+	`CREATE TABLE IF NOT EXISTS queries (
+		id INTEGER PRIMARY KEY,
+		query TEXT NOT NULL
+	);`,
+	`CREATE TABLE IF NOT EXISTS tokens (
+		id TEXT PRIMARY KEY,
+		token TEXT NOT NULL
+	);`,
+}
